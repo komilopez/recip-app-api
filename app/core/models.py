972e60e012ab233e6420780c6e87497edf3da188
Lopez Djagni <lopez.djagni@veone.net>
2023-10-11 19:54:55 +0000

    [add image]: feature add imege to recipe

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,11 @@
 """
 Database models.
 """
+
+import uuid
+import os
+
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -9,7 +14,13 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
-# Create your models here.
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -60,6 +71,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
